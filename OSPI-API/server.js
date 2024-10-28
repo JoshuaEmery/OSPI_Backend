89a09e794771f750628ebe08f97353a10dfc8ff0
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-28 15:48:22 -0700

    Project Update

--- OSPI-API/server.js
+++ OSPI-API/server.js
@@ -7,14 +7,14 @@ const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-console.log(process.env.TEST);
+//console.log(process.env.TEST);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(`Hello ${process.env.TEST}`);
 });
 
 app.listen(PORT, () => {
