3a63858373aa1bb68497d5b11802f9d234f7f717
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-28 14:52:15 -0700

    Project Setup

--- OSPI-API/server.js
+++ OSPI-API/server.js
@@ -1,1 +1,22 @@
-console.log("Test");
+//console.log("Test");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+console.log(process.env.TEST);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
